dd57c81bfccd060f8218537ee5637483f22bc198
season <sionwer4@naver.com>
2024-08-20 17:39:58 +0900

    feat: Create health check controller

---
+++ v1-email-password/login-back/src/main/java/com/example/loginback/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.example.loginback.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public String healthCheck(){
+        return "Server is running successfully";
+    }
+
+}
