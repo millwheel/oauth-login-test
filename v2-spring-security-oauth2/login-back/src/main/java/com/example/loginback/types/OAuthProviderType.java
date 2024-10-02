b25b3a3a0302cb5950017fe817eb389254f0f3be
season <sionwer4@naver.com>
2024-10-02 18:50:50 +0900

    feat: Add auth provider type

---
+++ v2-spring-security-oauth2/login-back/src/main/java/com/example/loginback/types/OAuthProviderType.java
@@ -0,0 +1,10 @@
+package com.example.loginback.types;
+
+public enum OAuthProviderType {
+
+    GOOGLE, NAVER, KAKAO;
+
+    public static OAuthProviderType fromName(String type){
+        return OAuthProviderType.valueOf(type);
+    }
+}
