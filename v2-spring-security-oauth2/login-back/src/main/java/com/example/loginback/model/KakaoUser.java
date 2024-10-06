ab3066e15d5240c0588fc895f60f79b7a61e549d
season <sionwer4@naver.com>
2024-10-06 15:44:41 +0900

    feat: Put the provider setting in application.yaml

--- v2-spring-security-oauth2/login-back/src/main/java/com/example/loginback/model/KakaoUser.java
+++ v2-spring-security-oauth2/login-back/src/main/java/com/example/loginback/model/KakaoUser.java
@@ -3,20 +3,23 @@ package com.example.loginback.model;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import java.util.Map;
+
 public class KakaoUser extends OAuth2ProviderUser {
 
     public KakaoUser(OAuth2User oAuth2User, ClientRegistration clientRegistration) {
-        super(oAuth2User.getAttributes(), oAuth2User, clientRegistration);
+        super((Map<String, Object>) oAuth2User.getAttributes().get("kakao_account"), oAuth2User, clientRegistration);
     }
 
-    // TODO You should fill in
+
     @Override
     public String getId() {
-        return "sample id";
+        return (String) getAttributes().get("id");
     }
 
     @Override
     public String getUsername() {
+        // TODO Activate the username of Kakao OAuth
         return "sample username";
     }
 }
