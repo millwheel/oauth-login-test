de0c57985e9523f47b7524f5c0e7edf81cf38a61
millwheel <sionwer4@naver.com>
2024-10-07 10:29:26 +0900

    feat: Copy the front app from v1

---
+++ v3-react-spring-security-oauth2/login-front/src/Logout.js
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { useEffect } from "react";
+import { clearCookie } from "./Cookie";
+
+const Logout = ({ setIsAuthenticated }) => {
+  const navigate = useNavigate();
+
+  const logoutHandler = async () => {
+    return await axios.post("http://localhost:8080/logout", {});
+  };
+
+  useEffect(() => {
+    logoutHandler()
+      .then((response) => {
+        console.log(response.data);
+        toast.success(response.data, {
+          toastId: "logoutSuccess1",
+        });
+        clearCookie("access_token");
+        setIsAuthenticated(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error(error.response?.data || error.message);
+        toast.error(error.response?.data || "Logout failed.");
+      });
+  }, []);
+};
+
+export default Logout;
