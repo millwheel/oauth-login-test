7bd6f0ec1753f6f5fed4beb498ec3e5628a15e81
season <sionwer4@naver.com>
2024-11-09 23:32:49 +0900

    refactor: remove unused import

--- v4-react-spring-security-token/login-back/src/main/java/com/example/loginback/security/OAuth2LoginSuccessHandler.java
+++ v4-react-spring-security-token/login-back/src/main/java/com/example/loginback/security/OAuth2LoginSuccessHandler.java
@@ -8,14 +8,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
