2eabea37ab7c7a1a1cb14004c026d0ef3f443399
season <sionwer4@naver.com>
2024-11-13 22:35:24 +0900

    refactor: 사용하지 않는 엔드포인트 제거

--- v5-react-flutter-spring/login-back/src/main/java/com/example/loginback/controller/AuthController.java
+++ v5-react-flutter-spring/login-back/src/main/java/com/example/loginback/controller/AuthController.java
@@ -7,10 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
 import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,21 +20,13 @@ public class AuthController {
     private final OAuth2AuthorizationRequestResolver authorizationRequestResolver;
 
     @GetMapping("/check")
-    public ResponseEntity<Map<String, Boolean>> checkAuth(Authentication authentication) {
-        Map<String, Boolean> response = new HashMap<>();
+    @ResponseStatus(HttpStatus.OK)
+    public Map<String, Boolean> checkAuth(Authentication authentication) {
+        Map<String, Boolean> authCheckMap = new HashMap<>();
         boolean isAuthenticated = authentication != null;
-        response.put("isAuthenticated", isAuthenticated);
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        authCheckMap.put("isAuthenticated", isAuthenticated);
+        return authCheckMap;
     }
 
-    @GetMapping("/login/{provider}")
-    public ResponseEntity<String> getAuthorizationUrl(@PathVariable String provider, HttpServletRequest request) {
-        OAuth2AuthorizationRequest authorizationRequest = authorizationRequestResolver.resolve(request, provider);
-        if (authorizationRequest != null) {
-            String authorizationUrl = authorizationRequest.getAuthorizationUri();
-            return ResponseEntity.ok(authorizationUrl);
-        }
-        return ResponseEntity.badRequest().body("Invalid provider or request");
-    }
 
 }
